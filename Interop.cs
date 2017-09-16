02a10acf71c9983ca426f875f961bf6d663e7aea
sasagolang <wuzhengmeng@sina.com>
2017-09-16 10:33:47 +0800

    create
    create

---
+++ Interop.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    class Interop
+    {
+        public static IntPtr WTS_CURRENT_SERVER_HANDLE = IntPtr.Zero;
+
+        public static void ShowMessageBox(string message, string title)
+        {
+            int resp = 0;
+            WTSSendMessage(
+                WTS_CURRENT_SERVER_HANDLE,
+                WTSGetActiveConsoleSessionId(),
+                title, title.Length,
+                message, message.Length,
+                0, 0, out resp, false);
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern int WTSGetActiveConsoleSessionId();
+
+        [DllImport("wtsapi32.dll", SetLastError = true)]
+        public static extern bool WTSSendMessage(
+            IntPtr hServer,
+            int SessionId,
+            String pTitle,
+            int TitleLength,
+            String pMessage,
+            int MessageLength,
+            int Style,
+            int Timeout,
+            out int pResponse,
+            bool bWait);
+        const int FORMAT_MESSAGE_FROM_SYSTEM = 0x1000;
+        const int FORMAT_MESSAGE_IGNORE_INSERTS = 0x200;
+        const int FORMAT_MESSAGE_ALLOCATE_BUFFER= 0x100;
+        public const uint MAXIMUM_ALLOWED = 0x2000000;
+        public const int CREATE_NEW_CONSOLE = 0x00000010;
+        public const int CREATE_UNICODE_ENVIRONMENT = 0x00000400;
+        public const int NORMAL_PRIORITY_CLASS = 0x20;
+        public static void CreateProcess(string app, string path)
+        {
+            bool result;
+            IntPtr hToken = WindowsIdentity.GetCurrent().Token;
+            IntPtr hDupedToken = IntPtr.Zero;
+
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+            SECURITY_ATTRIBUTES sa = new SECURITY_ATTRIBUTES();
+            sa.Length = Marshal.SizeOf(sa);
+
+            STARTUPINFO si = new STARTUPINFO();
+            si.cb = Marshal.SizeOf(si);
+
+            int dwSessionID = WTSGetActiveConsoleSessionId();
+            result = WTSQueryUserToken(dwSessionID, out hToken);
+            ShowMessageBox("dwSessionID", dwSessionID.ToString());
+            if (!result)
+            {
+                ShowMessageBox("WTSQueryUserToken failed", "AlertService Message");
+            }
+
+            result = DuplicateTokenEx(
+                  hToken,
+                  GENERIC_ALL_ACCESS,
+                  ref sa,
+                  (int)SECURITY_IMPERSONATION_LEVEL.SecurityIdentification,
+                  (int)TOKEN_TYPE.TokenPrimary,
+                  ref hDupedToken
+               );
+
+            if (!result)
+            {
+                ShowMessageBox("DuplicateTokenEx failed", "AlertService Message");
+            }
+
+            IntPtr lpEnvironment = IntPtr.Zero;
+            result = CreateEnvironmentBlock(ref lpEnvironment, hDupedToken, false);
+
+            if (!result)
+            {
+                ShowMessageBox("CreateEnvironmentBlock failed", "AlertService Message");
+            }
+            int dwCreationFlags = NORMAL_PRIORITY_CLASS | CREATE_NEW_CONSOLE | CREATE_UNICODE_ENVIRONMENT;
+            result = CreateProcessAsUser(
+                                 hDupedToken,
+                                System.IO.Path.Combine(path,app),
+                                 String.Empty,
+                                 ref sa, ref sa,
+                                 false, dwCreationFlags,  lpEnvironment,
+                                 path, ref si, ref pi);
+
+            if (!result)
+            {
+                int error = Marshal.GetLastWin32Error();
+                string message = String.Format("CreateProcessAsUser Error: {0}", error);
+                ShowMessageBox(message, "AlertService Message");
+                IntPtr lpBuff = IntPtr.Zero;
+                string sMsg = "";
+                if (0 != FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER
+           | FORMAT_MESSAGE_FROM_SYSTEM
+           | FORMAT_MESSAGE_IGNORE_INSERTS,
+           IntPtr.Zero,
+           error,
+           0,
+           ref lpBuff,
+           0,
+           IntPtr.Zero))
+                {
+                    sMsg = Marshal.PtrToStringUni(lpBuff);            //结果为“重叠 I/O 操作在进行中”，完全正确  
+                    ShowMessageBox(sMsg, "AlertService Message");
+                    Marshal.FreeHGlobal(lpBuff);
+                }
+            }
+
+            if (pi.hProcess != IntPtr.Zero)
+                CloseHandle(pi.hProcess);
+            if (pi.hThread != IntPtr.Zero)
+                CloseHandle(pi.hThread);
+            if (hDupedToken != IntPtr.Zero)
+                CloseHandle(hDupedToken);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public Int32 dwProcessID;
+            public Int32 dwThreadID;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public Int32 Length;
+            public IntPtr lpSecurityDescriptor;
+            public bool bInheritHandle;
+        }
+
+        public enum SECURITY_IMPERSONATION_LEVEL
+        {
+            SecurityAnonymous,
+            SecurityIdentification,
+            SecurityImpersonation,
+            SecurityDelegation
+        }
+
+        public enum TOKEN_TYPE
+        {
+            TokenPrimary = 1,
+            TokenImpersonation
+        }
+
+        public const int GENERIC_ALL_ACCESS = 0x10000000;
+
+        [DllImport("kernel32.dll", SetLastError = true,
+            CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool CloseHandle(IntPtr handle);
+
+        [DllImport("advapi32.dll", SetLastError = true,
+            CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool CreateProcessAsUser(
+            IntPtr hToken,
+            string lpApplicationName,
+            string lpCommandLine,
+            ref SECURITY_ATTRIBUTES lpProcessAttributes,
+            ref SECURITY_ATTRIBUTES lpThreadAttributes,
+            bool bInheritHandle,
+            Int32 dwCreationFlags,
+            IntPtr lpEnvrionment,
+            string lpCurrentDirectory,
+            ref STARTUPINFO lpStartupInfo,
+            ref PROCESS_INFORMATION lpProcessInformation);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        static public extern int FormatMessage(int dwFlags, IntPtr lpSource,
+                                int dwMessageId, int dwLanguageZId,
+                                ref IntPtr lpBuffer, int nSize, IntPtr Arguments);
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool DuplicateTokenEx(
+            IntPtr hExistingToken,
+            Int32 dwDesiredAccess,
+            ref SECURITY_ATTRIBUTES lpThreadAttributes,
+            Int32 ImpersonationLevel,
+            Int32 dwTokenType,
+            ref IntPtr phNewToken);
+
+        [DllImport("wtsapi32.dll", SetLastError = true)]
+        public static extern bool WTSQueryUserToken(
+            Int32 sessionId,
+            out IntPtr Token);
+
+        [DllImport("userenv.dll", SetLastError = true)]
+        static extern bool CreateEnvironmentBlock(
+            ref IntPtr lpEnvironment,
+            IntPtr hToken,
+            bool bInherit);
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Interop.ShowMessageBox("This a message from AlertService.",
+            //          "AlertService Message");
+            Interop.CreateProcess("1.bat", @"C:\Debug\");
+            //  ApplicationLoader.PROCESS_INFORMATION aa;
+            // ApplicationLoader.StartProcessAndBypassUAC(@"C:\Debug\1.bat", out  aa);
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
